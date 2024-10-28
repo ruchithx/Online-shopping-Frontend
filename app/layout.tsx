bbb0268af0aafc3ccda89527ed47dbfea9cb1754
MADHUSHANKA S.G.R.N <124671914+ruchithx@users.noreply.github.com>
2024-10-28 21:31:25 +0530

    feat:add route

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import './globals.css';
+import React from 'react';
+
+// import './globals.css';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
