2f6a85bb4d7310f13e2a382ab1c368de5c76685e
Ruwanthi Lakshika <125971277+RuwanthiLakshika@users.noreply.github.com>
2024-11-15 17:09:27 +0530

    Feature user profile (#2)
    * fix: interface for user profile
    
    * fix: interface for order
    
    * fix: final changes in change_password interface
    
    ---------
    
    Co-authored-by: MADHUSHANKA S.G.R.N <124671914+ruchithx@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,9 @@ import React from 'react';
 import Navbar from '@/public/components/NavBar';
 
 export default function Home() {
-
   return (
     <div>
       <Navbar />
     </div>
   );
-
 }
---
+++ app/user/profile/change-password/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ChangePassword from '@/components/auth/ChangePassword';
+
+const UserChangePassword = () => {
+  return (
+    <>
+    <ChangePassword />
+    </>
+    
+  )
+};
+
+export default UserChangePassword;
