c18dd0e25e648185768ae599f7bbd03acaa95c67
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-12-03 00:32:08 +0530

    feat: remove discount

--- app/cart/_components/CartPage.tsx
+++ app/cart/_components/CartPage.tsx
@@ -77,11 +77,11 @@ const CartPage: React.FC = () => {
       0,
     );
     const total = items.reduce(
-      (sum, item) => sum + (item.price * item.quantity - item.discount),
+      (sum, item) => sum + item.price * item.quantity,
       0,
     );
     const discount = items.reduce(
-      (sum, item) => sum + item.discount * item.quantity,
+      (sum, item) => sum + item.price * item.quantity,
       0,
     );
 
