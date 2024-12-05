c88bc7cdf2d4ed2174048ae07ed88821c2bfa847
Pramoth Dilshan <pramothdilshan2001@gmail.com>
2024-12-05 12:56:30 +0530

    resolve the build issue

--- app/api/keycloak/user/[id]/route.ts
+++ app/api/keycloak/user/[id]/route.ts
@@ -1,17 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 import { getAdminToken } from '@/utils/keycloak';
+import url from 'url';
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string } },
-) {
+export async function GET(req: NextRequest) {
   try {
     const adminToken = await getAdminToken();
-    const { id } = params;
+
+    const params = url.parse(req.url, true).query;
 
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_KEYCLOAK_HOST_URL}/admin/realms/${process.env.NEXT_PUBLIC_KEYCLOAK_REALM}/users/${id}`,
+      `${process.env.NEXT_PUBLIC_KEYCLOAK_HOST_URL}/admin/realms/${process.env.NEXT_PUBLIC_KEYCLOAK_REALM}/users/${params.id}`,
       {
         headers: {
           Authorization: `Bearer ${adminToken}`,
